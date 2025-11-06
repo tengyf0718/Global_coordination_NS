2dd09c26e41b0dca643b6917b340f493207f504b
tengyf0718 <tengyf0718@gmail.com>
2025-11-06 14:31:58 +0800

    Update reproject.py

--- reproject.py
+++ reproject.py
@@ -1,8 +1,9 @@
 import arcpy
 
-in_shp = r"F:\Yifan Teng\Data_new\MID_DATA.gdb\GUB2018_50_NS_sub_CounAndConti"
-out_shp = r"F:\Yifan Teng\Data_new\MID_DATA.gdb\GUB2018_50_NS_sub_CounAndConti_Moll"
-tif = r"F:\Yifan Teng\Data_new\GHS_BUILT_H_AGBH_E2018_GLOBE_R2023A_54009_100_V1_0\GHS_BUILT_H_AGBH_E2018_GLOBE_R2023A_54009_100_V1_0.tif"
+in_shp = r"F:\Data_new\MID_DATA.gdb\GUB2018_50_NS_sub_CounAndConti"
+out_shp = r"F:\Data_new\MID_DATA.gdb\GUB2018_50_NS_sub_CounAndConti_Moll"
+tif = r"F:\Data_new\GHS_BUILT_H_AGBH_E2018_GLOBE_R2023A_54009_100_V1_0\GHS_BUILT_H_AGBH_E2018_GLOBE_R2023A_54009_100_V1_0.tif"
 
 sr = arcpy.Describe(tif).spatialReference
 arcpy.management.Project(in_shp, out_shp, sr)
+
